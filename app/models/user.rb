c721cb5b07e2a007da45367c32f38f0bef6234bc
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-02-11 13:20:35 +0000

    setup styling for login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
   validates :first_name, presence: true, length: { minimum: 3, maximum: 25 }
   validates :last_name, presence: true, length: { minimum: 3, maximum: 25 }
 end
