60d42ce4e4cf57bbad2ccdfad93f4e5bc5a84a89
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-02-18 22:04:35 +0000

    add validation to company and oppurtunity

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,11 @@ class Company < ActiveRecord::Base
 
   has_many :opportunities, dependent: :destroy
 
-  validates :name, length: {minimum: 3, maximum: 15}
+  validates :name, presence: true, length: {minimum: 3}
+  validates :website, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 15, maximum: 300}
+  validates :company_size, presence: true
+  validates :founded, presence: true, length: {minimum: 4, maximum: 4}
+  validates :headquarters, presence: true, length: {minimum: 3}
+
 end
--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,3 +1,11 @@
 class Opportunity < ActiveRecord::Base
   belongs_to :company
+
+  validates :name, presence: true, length: {minimum: 3}
+  validates :location, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 15, maximum: 300}
+  validates :responsibilities, presence: true, length: {minimum: 15, maximum: 300}
+  validates :duration, presence: true, length: {minimum: 3}
+  validates :hourly_rate, presence: true
+
 end
