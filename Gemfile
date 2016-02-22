304a8c6bc83a8118ce86f82c504892a07f564142
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-02-22 02:32:35 +0000

    add better styling to internship cards

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem "haml-rails", "~> 0.9"
 
 gem "figaro"
 
+gem "font-awesome-rails"
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -3,8 +3,8 @@ class Opportunity < ActiveRecord::Base
 
   validates :name, presence: true, length: {minimum: 3}
   validates :location, presence: true, length: {minimum: 3}
-  validates :description, presence: true, length: {minimum: 15, maximum: 300}
-  validates :responsibilities, presence: true, length: {minimum: 15, maximum: 300}
+  validates :description, presence: true, length: {minimum: 75, maximum: 1000}
+  validates :responsibilities, presence: true, length: {minimum: 75, maximum: 1000}
   validates :duration, presence: true, length: {minimum: 3}
   validates :hourly_rate, presence: true
 
