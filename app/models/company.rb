e4d891134db8d4313dfd9d1f38b13458acfd7255
Levin George <levingeorge41@gmail.com>
2016-04-26 00:33:15 +0100

    created a migration within company table

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,9 @@ class Company < ActiveRecord::Base
 
   has_many :opportunities, dependent: :destroy
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/black.jpg"
+
+
   validates :name, presence: true, length: {minimum: 3}
   validates :website, presence: true, length: {minimum: 3}
   validates :description, presence: true, length: {minimum: 75, maximum: 1000}
